bd1329d096b7ac5443295e3d56c20977809c79f5
jtillil <77788257+jtillil@users.noreply.github.com>
2023-08-26 17:48:17 +0200

    &

--- src/TreeClassificationGroup.h
+++ src/TreeClassificationGroup.h
@@ -54,13 +54,13 @@ public:
   }
 
 private:
-  bool splitNodeInternal(size_t nodeID, std::vector<size_t>& possible_split_groupIDs, std::string& splitmethod) override;
+  bool splitNodeInternal(size_t nodeID, std::vector<size_t> possible_split_groupIDs, std::string& splitmethod) override;
   void createEmptyNodeInternal() override;
 
   double computePredictionAccuracyInternal(std::vector<double>* prediction_error_casewise) override;
 
   // Called by splitNodeInternal(). Sets split_groupIDs and split_values.
-  bool findBestSplit(size_t nodeID, std::vector<size_t>& possible_split_groupIDs, std::string& splitmethod);
+  bool findBestSplit(size_t nodeID, std::vector<size_t> possible_split_groupIDs, std::string& splitmethod);
   void findBestSplitValueSmallQ(size_t nodeID, size_t varID, size_t num_classes,
       const std::vector<size_t>& class_counts, size_t num_samples_node, double& best_value, size_t& best_varID,
       double& best_decrease);
