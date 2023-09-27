d6fe1e3d37f56873f065be9d380f895691e647f8
jtillil <77788257+jtillil@users.noreply.github.com>
2023-09-27 11:17:18 +0200

    Update DataSparse.cpp

--- src/DataSparse.cpp
+++ src/DataSparse.cpp
@@ -1,44 +1,44 @@
-/*-------------------------------------------------------------------------------
- This file is part of Ranger.
+// /*-------------------------------------------------------------------------------
+//  This file is part of Ranger.
 
- Ranger is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+//  Ranger is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 
- Ranger is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+//  Ranger is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with Ranger. If not, see <http://www.gnu.org/licenses/>.
+//  You should have received a copy of the GNU General Public License
+//  along with Ranger. If not, see <http://www.gnu.org/licenses/>.
 
- Written by:
+//  Written by:
 
- Marvin N. Wright
- Institut für Medizinische Biometrie und Statistik
- Universität zu Lübeck
- Ratzeburger Allee 160
- 23562 Lübeck
- Germany
+//  Marvin N. Wright
+//  Institut für Medizinische Biometrie und Statistik
+//  Universität zu Lübeck
+//  Ratzeburger Allee 160
+//  23562 Lübeck
+//  Germany
 
- http://www.imbs-luebeck.de
- #-------------------------------------------------------------------------------*/
+//  http://www.imbs-luebeck.de
+//  #-------------------------------------------------------------------------------*/
 
-#include "DataSparse.h"
+// #include "DataSparse.h"
 
-namespace ranger {
+// namespace ranger {
 
-DataSparse::DataSparse(Eigen::SparseMatrix<double>& x, Rcpp::NumericMatrix& y, std::vector<std::string> variable_names, size_t num_rows,
-    size_t num_cols) :
-    x { }{
-  this->x.swap(x);
-  this->y = y;
-  this->variable_names = variable_names;
-  this->num_rows = num_rows;
-  this->num_cols = num_cols;
-  this->num_cols_no_snp = num_cols;
-}
+// DataSparse::DataSparse(Eigen::SparseMatrix<double>& x, Rcpp::NumericMatrix& y, std::vector<std::string> variable_names, size_t num_rows,
+//     size_t num_cols) :
+//     x { }{
+//   this->x.swap(x);
+//   this->y = y;
+//   this->variable_names = variable_names;
+//   this->num_rows = num_rows;
+//   this->num_cols = num_cols;
+//   this->num_cols_no_snp = num_cols;
+// }
 
-} // namespace ranger
+// } // namespace ranger
