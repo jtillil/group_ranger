9bf654412de23921a7ebe13d9b66e5abe6c75692
jtillil <77788257+jtillil@users.noreply.github.com>
2023-09-27 10:36:28 +0200

    dbg

--- src/DataFloat.h
+++ src/DataFloat.h
@@ -65,9 +65,9 @@ public:
   }
 
   // WRITTEN FOR GROUP RANGER IMPLEMENTATION
-  std::vector<std::vector<double>> get_x_subset(std::vector<size_t> row, std::vector<uint> col, bool* debug) const override = {};
-  std::vector<double> get_x_sample(size_t row, std::vector<uint> col, bool* debug) const override = {};
-  std::vector<double> get_y_subset(std::vector<size_t> row) const override {};
+  // std::vector<std::vector<double>> get_x_subset(std::vector<size_t> row, std::vector<uint> col, bool* debug) const override = {};
+  // std::vector<double> get_x_sample(size_t row, std::vector<uint> col, bool* debug) const override = {};
+  // std::vector<double> get_y_subset(std::vector<size_t> row) const override {};
 
 private:
   std::vector<float> x;
