a858048f27b5d8de37a624950a964c87e22fad81
jtillil <77788257+jtillil@users.noreply.github.com>
2023-09-27 11:50:38 +0200

    Update TreeClassificationGroup.cpp

--- src/TreeClassificationGroup.cpp
+++ src/TreeClassificationGroup.cpp
@@ -333,7 +333,7 @@ void TreeClassificationGroup::findBestSplitValueUnordered(size_t nodeID, size_t 
   for (size_t pos = start_pos[nodeID]; pos < end_pos[nodeID]; ++pos) {
     size_t sampleID = sampleIDs[pos];
 
-    if (x_is_in_right_child_hyperplane((data->get_x_subset({sampleID}, group))[0], hyperplane)) {
+    if (x_is_in_right_child_hyperplane((data->get_x_subset({sampleID}, group, *debug))[0], hyperplane)) {
       uint sample_classID = (*response_classIDs)[sampleID];
       ++class_counts_right[sample_classID];
       ++n_right;
